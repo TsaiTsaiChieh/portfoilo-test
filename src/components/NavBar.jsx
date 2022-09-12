eedfed793fa9799a9457490d56ff8dabd6543c2f
Tsai-Chieh <d0177381@gmail.com>
2022-09-12 08:39:42 +0800

    :sparkles: add img source

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import {Navbar, Nav, Container} from 'react-bootstrap'
+import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 const NavBar = () => {
   return (
     <Navbar expand='lg'>
       <Container>
         <Navbar.Brand href='#home'>
-          <img src='' alt='logo' />
+          <img src={logo} alt='logo' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
@@ -16,9 +20,9 @@ const NavBar = () => {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-              <a href='/'><img src='' alt='' /></a>
-              <a href='/'><img src='' alt='' /></a>
-              <a href='/'><img src='' alt='' /></a>
+              <a href='/'><img src={navIcon1} alt='' /></a>
+              <a href='/'><img src={navIcon2} alt='' /></a>
+              <a href='/'><img src={navIcon3} alt='' /></a>
             </div>
             <button className='vvd' onClick={() => console.log('connect')}>
               <span>Let&apos;s connect</span>
