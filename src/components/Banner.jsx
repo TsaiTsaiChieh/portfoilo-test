71b6fc4a40a359d81c35637b1b48b69825dca7be
Tsai-Chieh <d0177381@gmail.com>
2022-09-12 15:26:38 +0800

    :sparkles: add text animations

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,45 @@
+import {useEffect, useState} from 'react'
 import {Col, Container, Row} from 'react-bootstrap'
 import {ArrowRightCircle} from 'react-bootstrap-icons'
 import headerImg from '../assets/img/header-img.svg'
 
 const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer']
+  const [text, setText] = useState('')
+  const [delta, setDelta] = useState(300 - Math.random() * 100)
+  const period = 2000
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick()
+    }, delta)
+
+    return () => clearInterval(ticker)
+  }, [text])
+
+  const tick = () => {
+    const i = loopNum % toRotate.length
+    const fullText = toRotate[i]
+    const updatedText = isDeleting ?
+      fullText.substring(0, text.length - 1) :
+      fullText.substring(0, text.length + 1)
+    setText(updatedText)
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2)
+    }
+    if (updatedText === fullText) {
+      setIsDeleting(true)
+      setDelta(period)
+    } else if (updatedText === '') {
+      setIsDeleting(false)
+      setLoopNum(loopNum + 1)
+      setDelta(500)
+    }
+  }
+
   return (
     <section id='home' className='banner'>
       <Container>
@@ -11,16 +48,21 @@ const Banner = () => {
             <span className='tagline'>Welcome to my Portfolio</span>
             <h1>
               {`Hi I'm webdecoded`}
-              <span className='wrap'>web developer</span>
+              <span className='wrap'>{text}</span>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Aliquid, aspernatur veritatis. Eos amet iure quibusdam, iste
                 architecto nemo voluptatem itaque ab dolor dolorum nobis?
                 Praesentium neque nam facilis ducimus voluptatum.
               </p>
-              <button onClick={() => {
-                console.log('connect')
-              }}>Let&apos;s connect<ArrowRightCircle size={25} /></button>
+              <button
+                onClick={() => {
+                  console.log('connect')
+                }}
+              >
+                Let&apos;s connect
+                <ArrowRightCircle size={25} />
+              </button>
             </h1>
           </Col>
           <Col xs={2} md={6} xl={5}>
