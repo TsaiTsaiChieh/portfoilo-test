aa45344c84e097f619e560ab5fa92acfe27d1fcf
Tsai-Chieh <d0177381@gmail.com>
2022-09-15 15:49:07 +0800

    :sparkles: change export method

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
 
-export const Footer = () => {
+const Footer = () => {
   return (
     <footer className='footer'>
       <Container>
@@ -33,3 +33,5 @@ export const Footer = () => {
     </footer>
   )
 }
+
+export default Footer
